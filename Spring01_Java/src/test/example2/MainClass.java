c323ecdfe51c4f1b3476deeed350027ac6b2d159
kimgyuri <kkr6495@naver.com>
2020-01-28 11:01:11 +0900

    init의 bean class수정, test.example3 생성 후 MainClass(class로 Weapon type의 참조값 가져오기) 작성

--- Spring01_Java/src/test/example2/MainClass.java
+++ Spring01_Java/src/test/example2/MainClass.java
@@ -11,7 +11,7 @@ public class MainClass {
 		// init.xml  문서를 해석해서 bean 을 생성한다. 
 		ApplicationContext context=
 				new ClassPathXmlApplicationContext("test/example2/init.xml");
-		//스프링이 관리하고 있는 객체중에서 "myWeapon" 이라는 이름의 객체의 
+		//스프링이 관리하고 있는 객체중에서 "myWeapon" 이라는 "이름"의 객체의 
 		//참조값을 가지고 와서 Weapon type 으로 casting 해서 변수에 담는다.
 		Weapon w1=(Weapon)context.getBean("myWeapon");
 		//Weapon type  객체를 이용해서  원하는 동작을 한다. 
