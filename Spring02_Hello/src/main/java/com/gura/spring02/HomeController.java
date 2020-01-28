2c5d0d123d6cfbc60991f481434c0dd66279a11e
kimgyuri <kkr6495@naver.com>
2020-01-28 17:59:46 +0900

    web.xml에 welcome-file-list설정, HomeController.java에 리스트생성후 request에 담고, home.jsp에서 EL로 리스트 출력, Spring02 .gitignore수정

--- Spring02_Hello/src/main/java/com/gura/spring02/HomeController.java
+++ Spring02_Hello/src/main/java/com/gura/spring02/HomeController.java
@@ -1,5 +1,10 @@
 package com.gura.spring02;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 /*
@@ -12,7 +17,17 @@ public class HomeController {
 	
 	// /home.do 요청이 왔을때 요청을 처리하게 하는 @RequestMapping 어노테이션
 	@RequestMapping("/home.do")
-	public String home() {
+	public String home(HttpServletRequest request) {
+		List<String> notice= new ArrayList<>();
+		notice.add("감기조심");
+		notice.add("코로나 조심");
+		notice.add("중국 나빠요");
+		notice.add("어쩌구");
+		notice.add("저쩌구");
+		
+		//data(모델)을 request에 담는다 > 포워드 이동한 페이지에서 사용하기 위해서
+		request.setAttribute("notice", notice);
+		
 		/*
 		 *  "home"을 리턴해주면
 		 *  "/WEB-INF/views" + "home" + ".jsp"와 같은 문자열이 만들어지고
--- Spring02_Hello/src/main/webapp/WEB-INF/views/home.jsp
+++ Spring02_Hello/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,5 +10,12 @@
 <body>
 	<h1>인덱스 페이지 입니다</h1>
 	<p>어쩌구...저쩌구...</p>
+	
+	<h2>공지사항</h2>
+	<ul>
+		<c:forEach var="tmp" items="${notice }">
+			<li>${tmp }</li>
+		</c:forEach>
+	</ul>
 </body>
 </html>
