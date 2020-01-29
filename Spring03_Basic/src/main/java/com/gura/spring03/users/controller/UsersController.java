5c2df69259f259f80051d9888faa9d680007d6a2
kimgyuri <kkr6495@naver.com>
2020-01-29 15:45:02 +0900

    Spring03 프로젝트에 로그인 요청 처리 2,3번째 방법 구현함+로그아웃 기능 구현

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -5,7 +5,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring03.users.dto.UsersDto;
 
 @Controller
 public class UsersController {
@@ -37,4 +40,59 @@ public class UsersController {
 		// view page의 정보를 리턴한다
 		return "users/login";
 	}
+	
+	/*
+	 * @RequestParam 어노테이션을 전송되는 파라미터를 자동으로 추출할떄 사용한다.
+	 * 단, 지역변수의 이름은 파라미터의 이름과 같아야한다.
+	 */
+	@RequestMapping("/users/login2")
+	public ModelAndView login2(@RequestParam String id, @RequestParam String pwd, HttpSession session, ModelAndView mView) {//파라미터명과 같으면 자동으로 추출가능
+		//유효한 정보인지 확인여부
+		boolean isSuccess=false;
+		if(id.equals("gura")&&pwd.equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 할 아이디를 세션에 담기
+			session.setAttribute("id", id);
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page의 정보도 담고
+		mView.setViewName("users/login");
+		//리턴해준다
+		return mView;
+	}
+	
+	@RequestMapping("/users/login3")
+	public ModelAndView login3(UsersDto dto,HttpSession session,ModelAndView mView) {
+		//유효한 정보인지 확인여부
+		boolean isSuccess=false;
+		if(dto.getId().equals("gura")&&dto.getPwd().equals("1234")) {
+			isSuccess=true;
+			//로그인 처리를 할 아이디를 세션에 담기
+			session.setAttribute("id", dto.getId());
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page의 정보도 담고
+		mView.setViewName("users/login");
+		//리턴해준다
+		return mView;
+	}
+	
+	@RequestMapping("/users/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "users/logout";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
--- Spring03_Basic/src/main/webapp/WEB-INF/views/users/loginform.jsp
+++ Spring03_Basic/src/main/webapp/WEB-INF/views/users/loginform.jsp
@@ -7,8 +7,24 @@
 <title>users/loginform.jsp</title>
 </head>
 <body>
-	<h1>로그인 폼 입니다</h1>
+	<h1>로그인 폼 입니다1</h1>
 	<form action="login.do" method="post">
+		<label for="id">아이디</label>
+		<input type="text" name="id" id="id" />
+		<label for="pwd">비밀번호</label>
+		<input type="password" name="pwd" id="pwd" />
+		<button type="submit">로그인</button>
+	</form>
+	<h1>로그인 폼 입니다2</h1>
+	<form action="login2.do" method="post">
+		<label for="id">아이디</label>
+		<input type="text" name="id" id="id" />
+		<label for="pwd">비밀번호</label>
+		<input type="password" name="pwd" id="pwd" />
+		<button type="submit">로그인</button>
+	</form>
+	<h1>로그인 폼 입니다3</h1>
+	<form action="login3.do" method="post">
 		<label for="id">아이디</label>
 		<input type="text" name="id" id="id" />
 		<label for="pwd">비밀번호</label>
