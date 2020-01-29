dd754c3989719397af8c70d44d29310355fdf94d
kimgyuri <kkr6495@naver.com>
2020-01-29 12:04:44 +0900

    Spring03 프로젝트에 할일 목록보기 두번째 링크 구현함

--- Spring03_Basic/src/main/java/com/gura/spring03/todo/controller/TodoController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/todo/controller/TodoController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 //1. controller 어노테이션
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TodoController {
 	//2. requestMapping 어노테이션으로 요청 맵핑하기
@@ -22,6 +23,21 @@ public class TodoController {
 		request.setAttribute("todoList", todoList);
 		//5. view page의 정보를 리턴하면 해당페이지로 forward이동해서 응답이 된다
 		return "todo/list";
+	}
+	
+	@RequestMapping("/todo/list2")// .do 생략가능
+	public ModelAndView list2() {//ModelAndView type은 모델의 정보와 뷰페이지의 정보를 같이 담아 전송된다. & HttpServletRequest를 선언해주지 않아도 된다
+		List<String> todoList= new ArrayList<String>();
+		todoList.add("html 공부하기");
+		todoList.add("css 공부하기");
+		todoList.add("javasript 공부하기");
 		
+		// model과 view page정보를 담을 수 있는 객체 생성
+		ModelAndView mView=new ModelAndView();
+		// ModelAndView 객체에 .addObject(key,value)로 담은 객체는 자동으로 request 객체에 담긴다
+		mView.addObject("todoList", todoList);
+		//view page 정보도 .setViewName() 메소드를 이용해서 담는다
+		mView.setViewName("todo/list");
+		return mView;	
 	}
 }
