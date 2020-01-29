fd617a093e2cf32f2f4ac339aefb575b5fd8803a
kimgyuri <kkr6495@naver.com>
2020-01-29 17:55:45 +0900

    Spring04 프로젝트 추가하고 @before AOP 적용예제 구현함

---
+++ Spring04_AOP_Java/src/test/aspect/PenAspect.java
@@ -0,0 +1,23 @@
+package test.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect//aspect 역할을 할수 있도록
+@Component//bean이 될수 있도록
+public class PenAspect {
+	/*
+	 * spring이 관리하는 객체의 메소드 중에서 return type은 void, 메소드 명은 write1 메소드에 전달되는 인자는 없는 메소드의 수행 이전에 할 작업
+	 */
+	@Before("execution(void write*())")//스프링이 관리하고 있는 bean들 중에서만 적용하여라
+	public void prepare() {
+		System.out.println("Pen을 준비해요~!");
+	}
+	@After("execution(void write*())")
+	public void finish() {
+		System.out.println("Pen을 닫아요~!");
+		System.out.println("");
+	}
+}
---
+++ Spring04_AOP_Java/src/test/mypac/WritingUtil.java
@@ -0,0 +1,20 @@
+package test.mypac;
+
+import org.springframework.stereotype.Component;
+/*
+ * [bean이 된다]
+ * - 해당 클래스로 객체가 생성이 되고 스프링 bean 컨테이너에서 관리가 된다
+ */
+// @Component 어노테이션을 붙여놓으면 컴포넌트 스캔을 했을때 bean이 된다
+@Component
+public class WritingUtil {
+	public void write1() {
+		System.out.println("편지를 써요");
+	}
+	public void write2() {
+		System.out.println("일기를 써요");
+	}
+	public void write3() {
+		System.out.println("소설을 써요");
+	}
+}
