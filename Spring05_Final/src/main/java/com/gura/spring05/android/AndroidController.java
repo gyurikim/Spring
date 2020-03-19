37e89f537b918605a42ea6e5096f7d1cc232f8e1
kimgyuri <kkr6495@naver.com>
2020-03-19 12:05:49 +0900

    안드로이드 > 사진 파일 업로드 기능,리스트기능에 대해 구현

--- Spring05_Final/src/main/java/com/gura/spring05/android/AndroidController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/android/AndroidController.java
@@ -1,9 +1,12 @@
 package com.gura.spring05.android;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,19 +14,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gura.spring05.member.dao.MemberDao;
 import com.gura.spring05.member.dto.MemberDto;
 
 @Controller
 public class AndroidController {
+	
 	@Autowired
 	private MemberDao memberDao;
+	@Autowired
+	private AndroidDao androidDao;
 	
 	/*
-	 * ["김구라","해골","원숭이"] 형식의 문자열이 응답되는 메소드
+	 * 	["김구라","해골","원숭이"] 형식의 문자열이 응답되는 메소드 
 	 */
-	//서버에 List<>를 보내면 ["xxx","xxx"] 이러한 문자열이 응답되고, 이 문자열은 안드로이드에서는 JSONArray로 응답된다
 	@ResponseBody
 	@RequestMapping("/android/getnames")
 	public List<String> getNames(){
@@ -31,69 +37,110 @@ public class AndroidController {
 		names.add("김구라");
 		names.add("해골");
 		names.add("원숭이");
-		names.add("김규리");
 		return names;
 	}
-	
 	/*
-	 * {"num":1,"name":"김구라","isMan":true}형식의 json문자열이 응답된다.
+	 *  {"num":1,"name":"김구라","isMan":true} 형식의 
+	 *  json 문자열이 응답된다. 
 	 */
 	@ResponseBody
 	@RequestMapping("/android/getdetail")
 	public Map<String, Object> getDetail(){
-		Map<String, Object> map= new HashMap<String, Object>();
+		Map<String, Object> map=new HashMap<String, Object>();
 		map.put("num", 1);
 		map.put("name", "김구라");
 		map.put("isMan", true);
 		return map;
 	}
-	
-	@ResponseBody//pom.xml문서에 작성해 두었던 '잭슨-데이터바인드' 라이브러리가 알아서 제이슨형식으로 데이터를 응답한다.
+	@ResponseBody
 	@RequestMapping("/android/getmember")
-	public List<Map<String,Object>> getMember() {
+	public List<Map<String, Object>> getMember(){
+		
 		return memberDao.getList2();
 	}
 	
 	@ResponseBody
-	@RequestMapping("/android/member/list")
-	public List<Map<String,Object>> memberGetList(){
+	@RequestMapping("/android/member/list.do")
+	public List<Map<String, Object>> memberGetList(){
+		
 		return memberDao.memberGetList();
 	}
-	
-	 /*
-	  * Dto를 리턴해도 @ResponseBody 어노테이션을 붙여도 Map을 리턴한것과 동일하게 응답된다
-	  * {"num":1,"name":"김구라","addr":"노량진"}
-	  */
+	/*
+	 *  Dto 를 리턴하고 @ResponseBody 어노 테에션을 붙여도 
+	 *  Map 을 리턴한것과 동일하게 응답된다.
+	 *  {"num":1,"name":"김구라","addr":"노량진"}
+	 */
 	@ResponseBody
 	@RequestMapping("/android/member/detail")
 	public MemberDto memberDetail(@RequestParam int num) {
+	
 		return memberDao.getData(num);
 	}
 	
 	@ResponseBody
 	@RequestMapping("/android/member/delete")
 	public String memberDelete(@RequestParam int num) {
+		
 		memberDao.delete(num);
-		return "{\"isSuccess\":true}"; 
-	}	
+		
+		return "{\"isSuccess\":true}";
+	}
 	/*
-	 * name=김구라&addr=노량진
-	 * 
-	 * Member dto=new MemberDto();
-	 * dto.setName(name);
-	 * dto.setAddr(addr);
-	 * > MemberDto의 필드명과 파라미터명이 같으면 알아서 dto에 값을 담아준다
+	 *   name=김구라&addr=노량진
+	 *   
+	 *   MemberDto dto=new MemberDto();
+	 *   dto.setName(name);
+	 *   dto.setAddr(addr);
 	 */
 	@ResponseBody
 	@RequestMapping("/android/member/insert")
 	public String memberInsert(@ModelAttribute MemberDto dto) {
 		memberDao.insert(dto);
-		return "{\"isSuccess\":true}"; //리턴값을 앱에서 직접적으로 사용하지는 않지만 뭐라도 응답해주어야하니깐 불리언값을 넣어준다
-	}	
+		return "{\"isSuccess\":true}";
+	}
+	@ResponseBody
+	@RequestMapping("/android/image/upload")
+	public String imageUpload(HttpServletRequest request,
+			ImageDto dto) {
+		//파일을 저장할 폴더의 절대 경로를 얻어온다.
+		String realPath=request.getServletContext().getRealPath("/upload");
+		//콘솔창에 테스트 출력
+		System.out.println(realPath);
+		
+		//MultipartFile 객체의 참조값 얻어오기
+		//FileDto 에 담긴 MultipartFile 객체의 참조값을 얻어온다.
+		MultipartFile mFile=dto.getMyFile();
+		//원본 파일명
+		String orgFileName=mFile.getOriginalFilename();
+		
+		//저장할 파일의 상세 경로
+		String filePath=realPath+File.separator;
+		//디렉토리를 만들 파일 객체 생성
+		File file=new File(filePath);
+		if(!file.exists()){//디렉토리가 존재하지 않는다면
+			file.mkdir();//디렉토리를 만든다.
+		}
+		//파일 시스템에 저장할 파일명을 만든다. (겹치치 않게)
+		String saveFileName=
+				System.currentTimeMillis()+orgFileName;
+		try{
+			//upload 폴더에 파일을 저장한다.
+			mFile.transferTo(new File(filePath+saveFileName));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		//FileDto 객체에 추가 정보를 담는다.
+		dto.setImagePath("/upload/"+saveFileName);
+		//FileDao 객체를 이용해서 DB 에 저장하기
+		androidDao.insert(dto);		
+		
+		return "{\"isSuccess\":true}";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/android/image/list")
+	public List<ImageDto> imageList(){
+		
+		return androidDao.getList();
+	}
 }
-
-
-
-
-
-
